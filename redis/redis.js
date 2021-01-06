9ea0acfc59f5aa5241f648311e59aa29ba2ced4d
WS <liuyb@wondershare.cn>
2021-01-06 16:53:01 +0800

    feat:redis连接参数

--- redis/redis.js
+++ redis/redis.js
@@ -5,7 +5,9 @@ var redis = new ioRedis({
     host: '127.0.0.1',           // Redis host
     family: 4,                   // 4 (IPv4) or 6 (IPv6)
     password: 'lyb171049',
-    db: 0                       // 数据库号
+    db: 0,                       // 数据库号
+    enableReadyCheck: true,
+    autoResubscribe: true
 });
 // 默认127.0.0.1:6379
 // redis 链接错误
