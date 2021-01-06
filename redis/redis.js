a05166b4965de123062fca8e74b4ff5fc34b1c3a
WS <liuyb@wondershare.cn>
2021-01-06 17:11:53 +0800

    feat:redis连接不配置密码

--- redis/redis.js
+++ redis/redis.js
@@ -1,9 +1,17 @@
 var ioRedis = require('ioredis');
 var logger = require('morgan');
-var redis = new ioRedis('redis://:lyb171049@127.0.0.1:6379/4');
+var redis = new ioRedis({
+    port: 6379,                  // Redis port
+    host: '127.0.0.1',           // Redis host
+    family: 4,                   // 4 (IPv4) or 6 (IPv6)
+    // password: 'lyb171049',
+    db: 0,                       // 数据库号
+    enableReadyCheck: true,
+    autoResubscribe: true
+});
 // 默认127.0.0.1:6379
 // redis 链接错误
 redis.on("error", function (error) {
-  console.log(error);
+    console.log(error);
 });
 exports.redis = redis;
